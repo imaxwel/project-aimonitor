8d35ff646eed629591afaf3f6bb9e6bdcbf5f49c
wph <491739727@qq.com>
2018-01-19 12:00:41 +0800

    初始建立版本

---
+++ src/main/java/per/wph/aimonitor/AimonitorApplication.java
@@ -0,0 +1,12 @@
+package per.wph.aimonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AimonitorApplication.class, args);
+	}
+}
---
+++ src/test/java/per/wph/aimonitor/AimonitorApplicationTests.java
@@ -0,0 +1,16 @@
+package per.wph.aimonitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AimonitorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
